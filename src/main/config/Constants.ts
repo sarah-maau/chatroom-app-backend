894e465a64f0267e9c2c1289badd8f60d1f17ae5
Sarah Maury <maurysarah@hotmail.fr>
2022-04-11 08:34:56 +0200

    cors origin

--- src/main/config/Constants.ts
+++ src/main/config/Constants.ts
@@ -1,5 +1,5 @@
 export const APPLICATION_CONSTANTS = {
-  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:3005',
   // eslint-disable-next-line no-magic-numbers
   PORT: Number(process.env.HTTP_PORT) || 8081,
   HOST: process.env.HTTP_HOST || '127.0.0.1'
